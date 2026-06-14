0721303be5cb43a1cab064550ed96cb5ab974237
Iain Emsley <iain.emsley@warwick.ac.uk>
2026-06-14 21:37:56 +0100

    Update app.py to begin linking to AZ

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import webview
+from androzoo import androzoo
 
 """
 An example of serverless app architecture
@@ -6,17 +7,9 @@ An example of serverless app architecture
 
 
 class Api:
-    def addItem(self, title):
+    def searchCatalogue(self, title):
         print(f'Added item {title}')
 
-    def removeItem(self, item):
-        print(f'Removed item {item}')
-
-    def editItem(self, item):
-        print(f'Edited item {item}')
-
-    def toggleItem(self, item):
-        print(f'Toggled item {item}')
 
     def toggleFullscreen(self):
         webview.windows[0].toggle_fullscreen()
